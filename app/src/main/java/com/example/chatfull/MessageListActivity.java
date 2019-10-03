7d67ce61471322263a9b21b6622888244d49a226
Sowmen Das <sowmendipta@gmail.com>
2019-10-03 15:30:48 +0600

    Adding reflector start

---
+++ app/src/main/java/com/example/chatfull/MessageListActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatfull;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MessageListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_list);
+    }
+}
