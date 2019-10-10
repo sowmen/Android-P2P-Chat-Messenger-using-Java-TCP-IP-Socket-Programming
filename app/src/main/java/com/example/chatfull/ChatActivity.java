3149e269c0c1a9243628bc2cf9ad4cc4d532846d
Sowmen Das <sowmendipta@gmail.com>
2019-10-10 23:11:52 +0600

    Added toolbar, single side color change

--- app/src/main/java/com/example/chatfull/ChatActivity.java
+++ app/src/main/java/com/example/chatfull/ChatActivity.java
@@ -2,36 +2,41 @@ package com.example.chatfull;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.util.Base64;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.android.colorpicker.ColorPickerDialog;
+import com.android.colorpicker.ColorPickerSwatch;
 import com.bumptech.glide.Glide;
-import com.squareup.picasso.Picasso;
 import com.stfalcon.chatkit.commons.ImageLoader;
 import com.stfalcon.chatkit.messages.MessagesList;
 import com.stfalcon.chatkit.messages.MessagesListAdapter;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 
-public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity implements ColorPickerSwatch.OnColorSelectedListener {
 
     private static final int PICK_FILE_REQUEST = 1;
     private static final int PICK_IMAGE_REQUEST = 2;
@@ -41,15 +46,17 @@ public class ChatActivity extends AppCompatActivity {
     private MessageReceiveServer messageReceiveServer;
 
     MessagesList messagesList;
-//    MessageInput input;
     protected final String senderId = "1";
-    private final User me = new User("1","SELF"); // Assign Self Username
+    private final User me = new User("1", "SELF"); // Assign Self Username
     MessagesListAdapter<Message> adapter;
     int cnt = 0;
 
     Button btnSend;
     ImageButton btnAttachement, btnImage;
     EditText input;
+
+    ColorPickerDialog colorPickerDialog;
+    RelativeLayout back_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,17 +67,27 @@ public class ChatActivity extends AppCompatActivity {
         user.setId("0");
         user.setName("ABAL"); // Assign user username
 
-        messageReceiveServer = new MessageReceiveServer(ShowInfoActivity.getSelfIpAddress(), ShowInfoActivity.getSelfPort(),this);
+        messageReceiveServer = new MessageReceiveServer(ShowInfoActivity.getSelfIpAddress(), ShowInfoActivity.getSelfPort(), this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(user.getName());
 
         this.messagesList = findViewById(R.id.messagesList);
 
         ImageLoader imageLoader = new ImageLoader() {
             @Override
             public void loadImage(ImageView imageView, @Nullable String url, @Nullable Object payload) {
-                byte[] byteArray = Base64.decode(url,Base64.DEFAULT);
+                byte[] byteArray = Base64.decode(url, Base64.DEFAULT);
+                imageView.setAdjustViewBounds(true);
+//                imageView.setMaxHeight(700);
+//                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
                 Glide.with(getApplicationContext())
                         .asBitmap()
                         .load(byteArray)
+                        .error(R.drawable.baseline_insert_drive_file_24)
+                        .thumbnail(0.25f)
+                        .sizeMultiplier(.60f)
                         .into(imageView);
             }
         };
@@ -81,19 +98,57 @@ public class ChatActivity extends AppCompatActivity {
         btnSend = findViewById(R.id.bt_send);
         btnAttachement = findViewById(R.id.bt_attachment);
         btnImage = findViewById(R.id.bt_image);
+
+        //Initialize color picker
+        back_view = findViewById(R.id.background_view);
+        TypedArray ta = getApplicationContext().getResources().obtainTypedArray(R.array.colors);
+        int[] colors = new int[ta.length()];
+        for (int i = 0; i < ta.length(); i++) {
+            colors[i] = ta.getColor(i, 0);
+        }
+        ta.recycle();
+        colorPickerDialog = new ColorPickerDialog();
+        colorPickerDialog.initialize(R.string.color_title, colors, colors[0], 5, colors.length);
+        colorPickerDialog.setOnColorSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.select_all:
+                Toast.makeText(getApplicationContext(), "Select All Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.background_button:
+                Toast.makeText(getApplicationContext(), "BG Clicked", Toast.LENGTH_SHORT).show();
+                colorPickerDialog.show(getFragmentManager(), "COLOR");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onColorSelected(int color) {
+        back_view.setBackgroundColor(color);
     }
 
     public void onBtnSendClick(View view) {
-        if(input.getText().toString() == null) return;
+        if (input.getText().toString() == null) return;
 
         Message message = new Message(Integer.toString(++cnt), me, input.getText().toString(), Calendar.getInstance().getTime());
         message.setIsImage(false);
         message.setFilename(null);
         adapter.addToStart(message, true);
 
-        sender = new SendMessage(user.getIpAddress(), user.getPort(), message,this);
+        sender = new SendMessage(user.getIpAddress(), user.getPort(), message, this);
         sender.execute();
-        Log.e("SEND",input.getText().toString());
+        Log.e("SEND", input.getText().toString());
         input.setText("");
     }
 
@@ -116,26 +171,26 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_FILE_REQUEST && data!=null) {
+        if (requestCode == PICK_FILE_REQUEST && data != null) {
             if (resultCode == RESULT_OK) {
                 Uri file = data.getData();
                 Toast.makeText(this, file.getPath() + "FILE", Toast.LENGTH_SHORT).show();
             }
-        }else if (requestCode == PICK_IMAGE_REQUEST && data!=null) {
+        } else if (requestCode == PICK_IMAGE_REQUEST && data != null) {
             if (resultCode == RESULT_OK) {
                 Uri file = data.getData();
                 Message message = new Message(Integer.toString(++cnt), me, null, Calendar.getInstance().getTime());
                 message.setFilename(getFileName(file));
                 try {
-                    message.setFile(getBytes(this,file));
+                    message.setFile(getBytes(this, file));
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Log.e("SEND_FILE","COULD NOT CONVERT TO BYTE");
+                    Log.e("SEND_IMAGE", "COULD NOT CONVERT TO BYTE");
                 }
                 message.setIsImage(true);
 
-                adapter.addToStart(message,true);
-                sender = new SendMessage(user.getIpAddress(), user.getPort(), message,this);
+                adapter.addToStart(message, true);
+                sender = new SendMessage(user.getIpAddress(), user.getPort(), message, this);
                 sender.execute();
 //                Toast.makeText(this, file.getPath() + "IMAGE", Toast.LENGTH_SHORT).show();
             }
@@ -168,7 +223,7 @@ public class ChatActivity extends AppCompatActivity {
      * get bytes array from Uri.
      *
      * @param context current context.
-     * @param uri uri fo the file to read.
+     * @param uri     uri fo the file to read.
      * @return a bytes array.
      * @throws IOException
      */
@@ -183,7 +238,6 @@ public class ChatActivity extends AppCompatActivity {
             } catch (IOException ignored) { /* do nothing */ }
         }
     }
-
 
 
     /**
@@ -207,31 +261,24 @@ public class ChatActivity extends AppCompatActivity {
             bytesResult = byteBuffer.toByteArray();
         } finally {
             // close the stream
-            try{ byteBuffer.close(); } catch (IOException ignored){ /* do nothing */ }
+            try {
+                byteBuffer.close();
+            } catch (IOException ignored) { /* do nothing */ }
         }
         return bytesResult;
     }
-//    @Override
-//    public boolean onSubmit(CharSequence input) {
-//        Message message = new Message(Integer.toString(++cnt), me, input.toString(), Calendar.getInstance().getTime());
-//        adapter.addToStart(message, true);
-//
-//        sender = new SendMessage(user.getIpAddress(), user.getPort(), input.toString(),this);
-//        sender.execute();
-//        return true;
-//    }
 
-    public void stopSender(){
-        if(sender != null)
+    public void stopSender() {
+        if (sender != null)
             sender.cancel(true);
     }
 
-    public void setMessage(final Message msg){
-        Log.e("IN_SET",msg.toString());
-        if(msg.getText()!= null && msg.getText().equalsIgnoreCase("OFFLINE")){
-            if(sender != null)
+    public void setMessage(final Message msg) {
+        Log.e("IN_SET", msg.toString());
+        if (msg.getText() != null && msg.getText().equalsIgnoreCase("OFFLINE")) {
+            if (sender != null)
                 sender.cancel(true);
-            if(messageReceiveServer != null)
+            if (messageReceiveServer != null)
                 messageReceiveServer.onDestroy();
             this.finish();
             return;
@@ -241,12 +288,12 @@ public class ChatActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if(msg.getText() != null) {
+                if (msg.getText() != null) {
                     Message message = new Message(Integer.toString(++cnt), user, msg.getText(), Calendar.getInstance().getTime());
                     adapter.addToStart(message, true);
-                } else if(msg.isImage()){
+                } else if (msg.isImage()) {
                     msg.setUser(user);
-                    adapter.addToStart(msg,true);
+                    adapter.addToStart(msg, true);
                 }
             }
         });
@@ -254,10 +301,10 @@ public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        Log.e("CHAT_ACTIVITY","DESTROY");
-        if(sender != null)
+        Log.e("CHAT_ACTIVITY", "DESTROY");
+        if (sender != null)
             sender.cancel(true);
-        if(messageReceiveServer != null)
+        if (messageReceiveServer != null)
             messageReceiveServer.onDestroy();
         super.onDestroy();
     }
@@ -265,7 +312,7 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         Log.e("CHAT_ACTIVITY", "PAUSE");
-        sender = new SendMessage(user.getIpAddress(), user.getPort(), new Message(Integer.toString(++cnt), me, "OFFLINE"),this);
+        sender = new SendMessage(user.getIpAddress(), user.getPort(), new Message(Integer.toString(++cnt), me, "OFFLINE"), this);
         sender.execute();
         try {
             Thread.sleep(500);
@@ -273,10 +320,11 @@ public class ChatActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         super.onBackPressed();
-        if(sender != null && !sender.isCancelled())
+        if (sender != null && !sender.isCancelled())
             sender.cancel(true);
-        if(messageReceiveServer != null)
+        if (messageReceiveServer != null)
             messageReceiveServer.onDestroy();
 
     }
+
 }
